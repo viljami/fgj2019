76f34c6a57fe5cb48be896e45ef15ebf606c774a
Viljami Peltola <viljami.peltola@iki.fi>
2019-01-26 13:23:49 +0200

    Clean assets, model stub

---
+++ src/model/node.js
@@ -0,0 +1,11 @@
+
+class Node {
+  constructor(name, owner, x, y, defence, spritePath) {
+    this.name = name;
+    this.owner = owner;
+    this.x = x;
+    this.y = y;
+    this.defence = defence
+    this.spritePath = spritePath;
+  }
+}
---
+++ src/model/path.js
@@ -0,0 +1,8 @@
+
+class Path {
+  constructor(node1, node2, parties = []) {
+    this.node1 = node1;
+    this.node2 = node2;
+    this.parties = parties;
+  }
+}
