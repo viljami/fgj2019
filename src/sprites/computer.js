64a5e46738101af53a27f92ce83b745b3ff1aaba
Sampo Tolvanen <sanepps7@gmail.com>
2019-01-26 15:23:39 +0200

    Add sprites

---
+++ src/sprites/computer.js
@@ -0,0 +1,14 @@
+class Computer {
+    constructor(model, scene) {
+        this.model = model;
+        this.circle = new Phaser.Geom.Circle(0, 0, 10);
+        this.graphics = scene.add.graphics({ lineStyle: { width: 3, color: 0xff00ff }, fillStyle: { color: 0x00ff00 } });
+        this.graphics.fillCircleShape(this.circle);
+    }
+
+    getSprite() {
+        return this.graphics;
+    }
+}
+
+export default Computer;
