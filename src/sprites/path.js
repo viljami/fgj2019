aa5f8f372ea17fd09c11f3cda9d003f4d29b994f
Viljami Peltola <viljami.peltola@iki.fi>
2019-01-26 15:29:48 +0200

    Set paths from nodes

--- src/sprites/path.js
+++ src/sprites/path.js
@@ -1,7 +1,7 @@
 class Path {
     constructor(model, scene, x1, y1, x2, y2) {
         this.model = model;
-        this.line = new Phaser.Geom.Line(0, 0, x1, y1, x2, y2);
+        this.line = new Phaser.Geom.Line(x1, y1, x2, y2);
         this.graphics = scene.add.graphics({ lineStyle: { width: 3, color: 0xff00ff }, fillStyle: { color: 0x00ff00 } });
         this.graphics.strokeLineShape(this.line);
     }
