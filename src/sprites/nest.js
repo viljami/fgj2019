691c32aa8137b319287180ce646efc7ec971f41f
Viljami Peltola <viljami.peltola@iki.fi>
2019-01-26 23:03:02 +0200

    Indicate player, indicate defences

--- src/sprites/nest.js
+++ src/sprites/nest.js
@@ -15,12 +15,13 @@ class Nest extends Node {
 
     update() {
         this.graphics.clear();
+        this.graphics.defaultFillColor = this.model.owner === 'player' ? 0x00ff00 : 0xff0000;
         this.circle.setTo(this.circle.x, this.circle.y, this.model.defence || 1);
         this.graphics.fillCircleShape(this.circle);
     }
 
     setupInput() {
-        var circle = new Phaser.Geom.Circle(0, 0, 25);
+        var circle = new Phaser.Geom.Circle(0, 0, this.model.defence < 25 ? 25 : this.model.defence);
         this.graphics.setInteractive(circle, Phaser.Geom.Circle.Contains);
     }
 
