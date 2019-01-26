7c2e5c32a4b759cf169f8c2ea4fac3f4585aad83
Sampo Tolvanen <sanepps7@gmail.com>
2019-01-26 14:02:00 +0200

    GameScene

---
+++ src/GameObjectCollection.js
@@ -0,0 +1,7 @@
+var GameObjectCollection = module.exports = {
+    nest: require('./sprites/nest.js').default,
+    worm: require('./sprites/worm.js').default,
+    cake: require('./sprites/cake.js').default,
+    computer: require('./sprites/computer.js').default,
+    snake: require('./sprites/snake.js').default
+}
--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,3 +1,6 @@
+var Path = require('../sprites/path.js').default;
+var GameObjectCollection = require('../GameObjectCollection.js')
+var Graph = require('../../assets/graph.json');
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -11,8 +14,26 @@ class GameScene extends Phaser.Scene {
     }
 
     create() {
+        this.loadGraph(Graph);
+    }
 
-
+    loadGraph(graph) {
+        var nodeMapping = {};
+        for (var node of graph.nodes) {
+            var objCtr = GameObjectCollection[node.name];
+            if (!objCtr) {
+                throw new Error('Unknown object type ' + node.name);
+            }
+            var obj = new objCtr(node, this);
+        
+            obj.getSprite().setPosition(node.x, node.y);
+            nodeMapping[node.name] = node;
+        }
+        for (var path of graph.paths) {
+            var node1 = nodeMapping[path.node1];
+            var node2 = nodeMapping[path.node2];
+            new Path(path, this, node1.x, node1.y, node2.x, node2.y);
+        }
     }
 }
 
