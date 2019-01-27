27e08b21937909febf6b6d269b8b467891bd9fa0
Sampo Tolvanen <sanepps7@gmail.com>
2019-01-27 11:15:33 +0200

    Change path asset

--- src/sprites/path.js
+++ src/sprites/path.js
@@ -5,11 +5,13 @@ class Path {
         var dx = x1-x2;
         var dy = y1-y2;
         var dist = Math.sqrt(dx*dx+dy*dy);
-        this.sprite = scene.add.tileSprite(x1, y1, dist, 50, 'trail');
+        var scale = 0.5;
+        this.sprite = scene.add.tileSprite(x1, y1, dist/scale, 200, 'path');
         var angle = Phaser.Math.Angle.Between(x1, y1, x2, y2);
         this.sprite.angle = angle * 180/Math.PI;
         this.sprite.displayOriginX = 0;
-        this.sprite.displayOriginY = 25;
+        this.sprite.displayOriginY = 115;
+        this.sprite.setScale(scale);
         this.setupInput();
     }
 
